603305b3d02a4766c20a81f7b1f8e572ec5a4fa5
David Maddison <david.maddison@science.oregonstate.edu>
2014-08-18 15:37:55 -0700

    Updated PackageIntro file

--- Source/mesquite/thinkTrees/aThinkTreesIntro/aThinkTreesIntro.java
+++ Source/mesquite/thinkTrees/aThinkTreesIntro/aThinkTreesIntro.java
@@ -28,11 +28,37 @@ public class aThinkTreesIntro extends PackageIntro {
 	/*.................................................................................................................*/
 	/** Returns citation for a package of modules*/
  	public String getPackageCitation(){
- 		return "Maddison, D.R., & W.P. Maddison.  2012.  ThinkTrees.  A package of modules for Mesquite. Version 0.2.";
+ 		return "Maddison, D.R., & W.P. Maddison.  2014.  ThinkTrees.  A package of Mesquite modules for teaching phylogenetics. " + getPackageVersion() + ".";
  	}
 	/*.................................................................................................................*/
 	/** Returns whether there is a splash banner*/
 	public boolean hasSplash(){
  		return false; 
 	}
+	/*.................................................................................................................*/
+	/** Returns version for a package of modules*/
+	public String getPackageVersion(){
+		return "0.3";
+	}
+	/*.................................................................................................................*/
+	/** Returns version for a package of modules as an integer*/
+	public int getPackageVersionInt(){
+		return 300;
+	}
+	/*.................................................................................................................*/
+	/** Returns build number for a package of modules as an integer*/
+	public int getPackageBuildNumber(){
+		return 10;
+	}
+/* release history:
+ 
+ 	0.3, build 10, 18 August 2014
+	
+	 */
+
+
+	public String getPackageDateReleased(){
+		return "18 August 2014";
+	}
+
 }
